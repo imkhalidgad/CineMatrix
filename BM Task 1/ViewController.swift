756c62f9b81e3035a0c69a29206b465a8ad8a18e
Mahmoud Abdelhady <eng.mahmoud.sh.77@gmail.com>
2024-07-19 16:06:09 +0300

    Initial Commit

---
+++ BM Task 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BM Task 1
+//
+//  Created by Apple on 19/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
